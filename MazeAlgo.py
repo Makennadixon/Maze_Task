6ea4e8356006bf3dfd64712a44252c3b8682021f
Duncan Tulimieri <87193935+tulimid1@users.noreply.github.com>
2021-07-27 19:22:35 -0400

    Add files via upload
    Converted live script to plain .m
    Initialized .py file

---
+++ MazeAlgo.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Makenna Maze Generation Algorithm 
+
+# %% Import libraries 
+import os 
+import matplotlib.pyplot as plt
+import numpy as np 
+
+# %% Set up 
+
+makenna_folder = ''
+devin_folder = ''
+duncan_folder = '/Users/duncan/OneDrive - University of Delaware - o365/Documents/Experiments/Makenna/Maze_Task-Convert-and-Optimize'
+
+os.chdir(duncan_folder)
+
+# %% Create grid 
+
+# %% Direction types and randomize directions 
+
+# %% Create path 
+
+# %% Finish area 
+
