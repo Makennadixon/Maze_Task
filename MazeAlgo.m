a6f5a7384eafbd185817742d5bda2ece3559b9eb
Makennadixon <68434810+Makennadixon@users.noreply.github.com>
2021-08-27 07:26:11 -0400

    Add files via upload

--- MazeAlgo.m
+++ MazeAlgo.m
@@ -1,23 +1,21 @@
 %% Makenna Maze Generation Algorithm and Example
-clear; clc; 
+% clear; clc; 
 % makenna_folder = '';
 % devin_folder = '';
-duncan_folder = '/Users/duncan/Documents/GitHub/Maze_Task';
+% duncan_folder = '/Users/duncan/Documents/GitHub/Maze_Task';
 % 
-cd(duncan_folder); 
+% cd(duncan_folder); 
 
 %% Call function 
 
-number_of_turns = 5;
-length_of_path = 10;
-gridX = 10; gridY = 10;
-maxIter = 100; 
+number_of_turns = 6;
+length_of_path = 15;
+gridX = 10; 
+gridY = 10;
+maxIter = 350;
 
-num_runs = 5; % use to make sure works all the time 
-
-for i = 1:num_runs
     genMaze(number_of_turns, length_of_path, gridX, gridY, maxIter); 
-end
+
 
 %% Function 
 
@@ -28,9 +26,9 @@ Generate random maze
 INPUTS:
 num_turns: # of turns 
 path_length: # of blocks / spaces to travel 
-gridX: x maximum 
-gridY: y maximum 
-maxIter: maximum number of iterations 
+gridX: x maximum
+gridY: y maximum
+maxIter: maximum number of iterations
 
 OUTPUT:
 Visualization of random maze 
@@ -51,6 +49,9 @@ arguments
 end
 
 %% Create grid
+is_new_path_good = false;
+currentIter = 1;
+while (is_new_path_good == false)
 
 % Create a 10 by 10 grid
 xgrid = 0:1:gridX;
@@ -62,25 +63,19 @@ startY = randi([1 gridY-1]);
 start_current_position = [startX, startY]; 
 
 % display 10 by 10 grid
-figure(); 
-arrayfun(@(x)xline(x,'k', "LineWidth",2), xgrid);
-arrayfun(@(y)yline(y,'k','LineWidth',2), ygrid);
+ figure(); 
+ arrayfun(@(x)xline(x,'k', "LineWidth",2), xgrid);
+ arrayfun(@(y)yline(y,'k','LineWidth',2), ygrid);
 hold on
 % display green start location on grid
-arrayfun(@(x)xline(x,'k', "LineWidth",2),xgrid), ...
+ arrayfun(@(x)xline(x,'k', "LineWidth",2),xgrid), ...
     (rectangle('Position',[startX,startY,1,1],'FaceColor','g'));
 
 arrayfun(@(y)yline(y,'k','LineWidth',2),ygrid,'UniformOutput', false), ...
     (rectangle('Position',[startX,startY,1,1],'FaceColor','g'));
 
-maze_position = start_current_position; 
-
+maze_position = start_current_position;
 %% Direction types & randomize directions
-
-% 1 -> north
-% 2 -> east
-% 3 -> south
-% 4 -> west
 
 % Direction types
 N = [0,1];
@@ -93,7 +88,6 @@ W = [-1,0];
 % when the x, y coordinates are on the bounds of the grid
 % Then it can only go a certain direction
 
-% all these should have 2 (x,y) statements otherwise they may get overwritten 
 if startX == 9 && startY == 9
     w = [0, 0, 0.50, 0.50]; 
     
@@ -120,7 +114,7 @@ elseif startX == 0 && startY == 9
     
 elseif startX ~= 9 || startX ~= 0 && ...
         startY ~= 9 || startY ~= 0
-    % When the start location is not on the outer bounds
+    % When the start location is within the outer bounds
     % can equally randomize the direction of the start grid path
     w = [0.25, 0.25, 0.25, 0.25];
     
@@ -128,8 +122,7 @@ end
 
 % Use datasample to randomize direction types based on where the path can
 % contiune on to next
-rand_dirc = random_choice([1 2 3 4], w); 
-% rand_dirc = datasample([1 2 3 4],1,"Weights",w);
+rand_dirc = random_choice([1 2 3 4], w);
 
 if rand_dirc == 1 % North
     new = start_current_position + N; 
@@ -141,9 +134,10 @@ elseif rand_dirc == 4 % west
     new = start_current_position + W; 
 end
 
-% maze positions 
-maze_position = [maze_position; new]; 
-
+%maze position
+maze_position = [maze_position; new];
+%array storing logical values of if parameters are being satisfied
+path_array = is_new_path_good +1;
 % Create the starting maze block (blue) on the grid
 rectangle('Position',[new,1,1], 'FaceColor','b');
 
@@ -180,43 +174,37 @@ for i = 1:length(c)
             % p(2) -> y coordinate of maze path
             % new_path_dirc -> new x,y coordinates used to create the maze on grid
 
-            is_new_path_good = false; currentIter = 1;
-            while (is_new_path_good == false)
-                rand_dirc = random_choice([1 2 3 4], [0.25 0.25 0.25 0.25]); 
-%                 rand_dirc = datasample([1 2 3 4],1,"Weights",[0.25, 0.25, 0.25, 0.25]);
-                % create path in north direction
-                if rand_dirc == 1
-                    new_path_dirc = p + N;
-                end
-                % create path in east direction
-                if rand_dirc == 2
-                    new_path_dirc = p + E;
-                end
-                % create path in south direction
-                if rand_dirc == 3
-                    new_path_dirc = p + S;
-                end
-                % create path in west direction
-                if rand_dirc == 4
-                    new_path_dirc = p + W;
-                end
-                if (new_path_dirc(1) < 0 || new_path_dirc(1) > 9 || ...
-                        new_path_dirc(2) < 0 || new_path_dirc(2) > 9) || ...
-                        sum(sum(new_path_dirc == maze_position, 2) == 2) >0
-                    is_new_path_good = false;
-                else
-                    is_new_path_good = true;
-                end
-                if currentIter == maxIter
-                    fprintf('Algorithm did not converge. Try again.\n')
-                    return
-                end
-                currentIter = currentIter +1; 
-            end
+% create path in north direction
+    if rand_dirc == 1
+    new_path_dirc = p + N;
+% create path in east direction
+    elseif rand_dirc == 2
+    new_path_dirc = p + E;
+% create path in south direction
+    elseif rand_dirc == 3
+    new_path_dirc = p + S;
+% create path in west direction
+    elseif rand_dirc == 4
+    new_path_dirc = p + W;
+    end
+    %checks for if path is within bounds or overlaps
+    if (new_path_dirc(1) < 0 || new_path_dirc(1) > 9 || new_path_dirc(2) < 0 || new_path_dirc(2) > 9) || sum(sum(new_path_dirc == maze_position, 2) == 2) > 0
+        is_new_path_good = false;
+    else
+        is_new_path_good = true;
+    end
+    
+    if currentIter == maxIter
+        fprintf('Algorithm did not converege. Try again.\n')
+        return
+    end 
+    currentIter = currentIter +1;
 
-        maze_position = [maze_position; new_path_dirc]; 
-        % create maze path on the grid
-        rectangle('Position',[new_path_dirc,1,1], 'FaceColor','b');
+
+maze_position = [maze_position; new_path_dirc];
+path_array = [path_array; is_new_path_good];
+% create maze path on the grid
+rectangle('Position',[new_path_dirc,1,1], 'FaceColor','b');
         end
 
         % When the element in array c is 1 and not the last segment
@@ -227,71 +215,64 @@ for i = 1:length(c)
             % update the value of p
             p = new_path_dirc;
 
-            % change direction E W
-            % when direction of path is either going north or south
-
             % new_path_dirc(1) -> x coordinate of maze path @ turn
             % new_path_dirc(2) -> y coordinate of maze path @ turn
             % new_path-dirc2 -> new x, y coordinates of maze path @ turn
-            is_new_path_good = false; currentIter = 1;
-            while (is_new_path_good == false)
-                rand_dirc = random_choice([1 2 3 4], [0.25 0.25 0.25 0.25]); 
-%                 rand_dirc = datasample([1 2 3 4],1,"Weights",[0.25 0.25 0.25 0.25]);
 
-                if rand_dirc == 1 || rand_dirc == 3
-                    % randomly chooses between going east or west
-                    dirc = random_choice([1 2 3 4], [0 0.50 0 0.50]); 
-%                     dirc = datasample([1 2 3 4],1,"Weights",[0 0.50 0 0.50]);
-                    if dirc == 2 % east
-                        new_path_dirc2 = new_path_dirc + E;
-                    elseif dirc == 4 % west
-                        new_path_dirc2 = new_path_dirc + W;
-                    end
-                    % update the value of varibale p (coordiantes of the path)
-                    p = new_path_dirc2;
-                    % update the value of variable rand-dirc (path direction of the maze)
-                    rand_dirc = dirc;
-                    % update the value of variable new_path_dirc
-                    new_path_dirc = p;
+            % change direction E W
+            % when direction of path is either going north or south
+            if rand_dirc == 1 || rand_dirc == 3
+                % randomly chooses between going east or west
+                dirc = random_choice([1 2 3 4], [0 0.50 0 0.50]);
+                if dirc == 2 % east
+                    new_path_dirc2 = new_path_dirc + E;
+                elseif dirc == 4 % west
+                    new_path_dirc2 = new_path_dirc + W;
+                end
+                % update the value of varibale p (coordiantes of the path)
+                p = new_path_dirc2;
+                % update the value of variable rand-dirc (path direction of the maze)
+                rand_dirc = dirc;
+                % update the value of variable new_path_dirc
+                new_path_dirc = p;
 
-                    % change direction N S
-                    % when direction of path is either going east or west
-                elseif rand_dirc == 2 || rand_dirc == 4
-                    % randomly choose between going north or south
-                    dirc = random_choice([1 2 3 4], [0.50 0 0.50 0]); 
-%                     dirc = datasample([1 2 3 4],1,"Weights",[0.50 0 0.50 0]);
-                    if dirc == 1 % north
-                        new_path_dirc2 = new_path_dirc + N;
-                    elseif dirc == 3 % south
-                        new_path_dirc2 = new_path_dirc + S;
-                    end
-                    % update the value of variable p (coordiantes of the path)
-                    p = new_path_dirc2;
-                    % update the value of variable rand_dirc (path direction of the maze)
-                    rand_dirc = dirc;
-                    % update the value of variable new_path_dirc
-                    new_path_dirc = p;
+                % change direction N S
+                % when direction of path is either going east or west
+            elseif rand_dirc == 2 || rand_dirc == 4
+                % randomly choose between going north or south
+                dirc = random_choice([1 2 3 4], [0.50 0 0.50 0]);
+                if dirc == 1 % north
+                    new_path_dirc2 = new_path_dirc + N;
+                elseif dirc == 3 % south
+                    new_path_dirc2 = new_path_dirc + S;
                 end
-                if (new_path_dirc2(1) < 0 || new_path_dirc2(1) > 9 ||...
-                        new_path_dirc2(2) < 0 || new_path_dirc2(2) > 9) || ...
-                        sum(sum(new_path_dirc2 == maze_position, 2) == 2) >0
-                    is_new_path_good = false;
-                else
-                    is_new_path_good = true;
-                end
-                if currentIter == maxIter
-                    fprintf('Algorithm did not converge. Try again.\n')
-                    return
-                end
-                currentIter = currentIter +1; 
+                % update the value of variable p (coordiantes of the path)
+                p = new_path_dirc2;
+                % update the value of variable rand_dirc (path direction of the maze)
+                rand_dirc = dirc;
+                % update the value of variable new_path_dirc
+                new_path_dirc = p;
             end
-            maze_position = [maze_position; new_path_dirc2]; 
+
+            if (new_path_dirc2(1) < 0 || new_path_dirc2(1) > 9 || new_path_dirc2(2) < 0 || new_path_dirc2(2) > 9) || sum(sum(new_path_dirc2 == maze_position, 2 ) == 2) > 0
+                is_new_path_good = false;
+            else
+                is_new_path_good = true;
+            end 
+            
+if currentIter == maxIter
+ fprintf('Algorithm did not converge. Try Aagin.\n')  
+ return
+end
+currentIter = currentIter +1;
+
+
+maze_position = [maze_position; new_path_dirc2];
+path_array = [path_array; is_new_path_good];
             % creates the path of maze on the grid
             rectangle('Position',[new_path_dirc2,1,1], 'FaceColor','b')
 
         end
-    end
-end
 
 
 % create the finish grid area
@@ -299,63 +280,66 @@ end
 % therefore will go off thr grid
 
 % update the value of variable new_path_dirc
-new_path_dirc2 = new_path_dirc;
-is_new_path_good = false; currentIter = 1;
-while (is_new_path_good == false)
-    rand_dirc = random_choice([1 2 3 4], [0.25 0.25 0.25 0.25]); 
-%     rand_dirc = datasample([1 2 3 4],1,"Weights",[0.25 0.25 0.25 0.25]);
+if c(i) == 1 && i == Max_lengths 
+    c(i) = c(i) - 1;
     if rand_dirc == 1 % north
         finish_location = new_path_dirc2 + N; 
     elseif rand_dirc == 2 % east
         finish_location = new_path_dirc2 + E;
-    elseif rand_dirc == 3 % west
-        finish_location = new_path_dirc2 + W; 
-    elseif rand_dirc == 4 % south
+    elseif rand_dirc == 3 % south
         finish_location = new_path_dirc2 + S; 
+    elseif rand_dirc == 4 % west
+        finish_location = new_path_dirc2 + W; 
     end
-    if (finish_location(1) < 0 || finish_location(1) > 9 ||...
-            finish_location(2) < 0 || finish_location(2) > 9) || ...
-            sum(sum(finish_location == maze_position, 2) == 2) >0
-        is_new_path_good = false;
-    else
-        is_new_path_good = true;
-    end
-    if currentIter == maxIter
-        fprintf('Algorithm did not converge. Try again.\n')
-        return
-    end
-    currentIter = currentIter +1; 
-end
-maze_position = [maze_position; finish_location]; 
-rectangle('Position',[finish_location,1,1], 'FaceColor',[0.5 0.5 0.5]);
     
+    x = path_array == 0;
+    
+        if (finish_location(1) < 0 || finish_location(1) > 9 || finish_location(2) < 0 || finish_location(2) > 9) || sum(sum(finish_location == maze_position, 2) == 2) >0 || any(x == 1)
+            is_new_path_good = false;
+            fprintf('path does not satisfy parameters')
+        else
+            is_new_path_good = true;
+            fprintf('Algorithm converged\n') 
+        end
+            if currentIter == maxIter
+                fprintf('Algorithm did not converge. Try again.\n')
+                return
+            end
+currentIter = currentIter +1; 
+
+maze_position = [maze_position; finish_location];
+path_array = [path_array; is_new_path_good];
+rectangle('Position',[finish_location,1,1], 'FaceColor',[0.5 0.5 0.5]);
+end  
+    end
+end
+end
 end
 
-function choice = random_choice(array, w)
+function choice = random_choice(array,w)
 %{
-Randomly choose 1 value from array with weights w (pseudo-uniform)
-
+Randomly choose 1 value form array with weights q (pseeudo-uniform)
 INPUTS:
-array: array of values [N X 1]
-w: weights [N X 1]
+array: array of values [ N X 1]
+w: weights [ N X 1]
 
-OUTPUT:
-choice: element from array 
+OUTPUTS:
+choice: element from array
 %}
 
-arguments 
+arguments
     array (:,1)
     w (:,1)
 end
 
 if length(array) ~= length(w)
-    error('Elements of array and w must have same lengths')
+    error('Elements of array and w must have smae lengths')
 end
 
-choices = array(w ~= 0);
+choices = array(w~= 0);
 
 rnd = randi([1 length(choices)], 1);
 
-choice = choices(rnd); 
+choice = choices(rnd);
 
 end
